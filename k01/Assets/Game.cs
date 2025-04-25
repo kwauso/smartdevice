a793afd43c217ff6de6fe27933c9c65001d18e76
kwauso <akiranieda@keio.jp>
2025-04-25 17:16:51 +0900

    update

---
+++ k01/Assets/Game.cs
@@ -0,0 +1,62 @@
+#nullable enable
+using GameCanvas;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// ゲームクラス。
+/// 学生が編集すべきソースコードです。
+/// </summary>
+public sealed class Game : GameBase
+{
+    // 変数の宣言
+    int time = 600;
+    int score = 0;
+
+    /// <summary>
+    /// 初期化処理
+    /// </summary>
+    public override void InitGame()
+    {
+        // キャンバスの大きさを設定します
+        gc.ChangeCanvasSize(360, 640);
+    }
+
+    /// <summary>
+    /// 動きなどの更新処理
+    /// </summary>
+    public override void UpdateGame()
+    {
+        // 起動からの経過時間を取得します
+        time -= 1;
+
+        if (time >= 0)
+        {
+            score += gc.PointerBeginCount;
+        }
+
+        if (gc.GetPointerDuration(0) >= 2)
+        {
+            time = 600;
+            score = 0;
+        }
+    }
+
+    /// <summary>
+    /// 描画の処理
+    /// </summary>
+    public override void DrawGame()
+    {
+        gc.ClearScreen();
+
+        if(time >= 0 ){
+        gc.DrawString("time:"+time,60,0);
+        }
+        else {
+        gc.DrawString("finished!!",60,0);
+        }
+
+        gc.DrawString("score:"+score,60,60);
+    }
+}
---
+++ k02/Assets/Game.cs
@@ -0,0 +1,89 @@
+#nullable enable
+using GameCanvas;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// ゲームクラス。
+/// 学生が編集すべきソースコードです。
+/// </summary>
+public sealed class Game : GameBase
+{
+    // 変数の宣言
+    int money;
+    const int CARD_TYPE = 10;
+    int[] card_count = new int[CARD_TYPE];
+    string[] card_name = 
+        {"A","B","C","D","E","F","G","H","I","J"};
+    bool isComplete;
+    int new_card;
+
+    /// <summary>
+    /// 初期化処理
+    /// </summary>
+    public override void InitGame()
+    {
+        resetValue(); // 初期化処理をまとめた関数を呼ぶ
+    }
+
+    /// <summary>
+    /// 動きなどの更新処理
+    /// </summary>
+    public override void UpdateGame()
+    {
+        if (gc.GetPointerFrameCount(0) == 1 && !isComplete) {
+            money -= 100;
+            new_card = gc.Random(0, 9);
+            card_count[new_card]++;
+            
+            isComplete = true;
+            for (int i = 0; i < CARD_TYPE; i++) {
+                if (card_count[i] == 0) {
+                    isComplete = false;
+                }
+            }
+        }
+
+        // 長押しでリセット
+        if (gc.GetPointerFrameCount(0) >= 120) {
+            resetValue();
+        }
+    }
+
+    /// <summary>
+    /// 描画の処理
+    /// </summary>
+    public override void DrawGame()
+    {
+        gc.ClearScreen();
+        gc.SetColor(0, 0, 0);
+        gc.SetFontSize(36);
+        gc.DrawString("money:" + money, 60, 40);
+
+        if (new_card >= 0) {
+            gc.DrawString("new:" + card_name[new_card], 60, 80);
+        }
+
+        for (int i = 0; i < CARD_TYPE; i++) {
+            gc.DrawString(card_name[i] + ":" + card_count[i], 60, 120 + i * 40);
+        }
+
+        if (isComplete) {
+            gc.DrawString("complete!!", 60, 520);
+        }
+    }
+
+    // 初期化処理をまとめたメソッド
+    void resetValue()
+    {
+        gc.ChangeCanvasSize(720, 1280);
+        gc.SetRandomSeed((uint)gc.CurrentTimestamp * 1024);
+        money = 10000;
+        for (int i = 0; i < CARD_TYPE; i++) {
+            card_count[i] = 0;
+        }
+        isComplete = false;
+        new_card = -1;
+    }
+}
