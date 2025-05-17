0f4b8a672307eaf44f3deb02483b7118091c583b
kwauso <akiranieda@keio.jp>
2025-05-17 12:26:23 +0900

    課題(2)まで完了

--- k04/Assets/Game.cs
+++ k04/Assets/Game.cs
@@ -4,13 +4,8 @@ using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-/// <summary>
-/// ゲームクラス。
-/// 学生が編集すべきソースコードです。
-/// </summary>
 public sealed class Game : GameBase
 {
-    // 変数の宣言
     const int BALL_NUM = 30;
     int[] ball_x = new int [BALL_NUM];
     int[] ball_y = new int [BALL_NUM];
@@ -20,59 +15,80 @@ public sealed class Game : GameBase
     int ball_w = 24;
     int ball_h = 24;
 
-    /// <summary>
-    /// 初期化処理
-    /// </summary>
+    int player_x = 304;
+    int player_y = 448;
+    int player_speed = 3;
+    int player_w = 32;
+    int player_h = 32;
+    GcImage player_img = GcImage.GR0;
+
     public override void InitGame()
     {
-        // キャンバスの大きさを設定します
         gc.SetResolution(640, 480);
-        for(int i =0 ; i < BALL_NUM ; i ++ )
+        for(int i = 0; i < BALL_NUM; i++)
         {
-        resetBall(i);
-        }
-    }
-
-    /// <summary>
-    /// 動きなどの更新処理
-    /// </summary>
-    public override void UpdateGame()
-    {
-        // 起動からの経過時間を取得します
-        for(int i =0 ; i < BALL_NUM ; i ++ )
-        {
-        ball_y[i] = ball_y[i] + ball_speed[i];
-        if(ball_y[i]> 480){
             resetBall(i);
         }
+    }
+
+    public override void UpdateGame()
+    {
+        for(int i = 0; i < BALL_NUM; i++)
+        {
+            ball_y[i] = ball_y[i] + ball_speed[i];
+            if(ball_y[i] > 480)
+            {
+                resetBall(i);
+            }
+        }
+
+        if(gc.GetPointerFrameCount(0) > 0)
+        {
+            var px = gc.GetPointerX(0);
+
+            if(px > 320)
+            {
+                player_x += player_speed;
+                player_img = GcImage.GR0;
+            }
+            else
+            {
+                player_x -= player_speed;
+                player_img = GcImage.GL0;
+            }
         }
     }
 
-    /// <summary>
-    /// 描画の処理
-    /// </summary>
     public override void DrawGame()
     {
         gc.ClearScreen();
 
-        for(int i =0 ; i < BALL_NUM ; i ++ ){
-            gc.DrawImage(ball_img[i],ball_x[i],ball_y[i]);
+        for(int i = 0; i < BALL_NUM; i++)
+        {
+            gc.DrawImage(ball_img[i], ball_x[i], ball_y[i]);
         }
+
+        gc.DrawImage(player_img, player_x, player_y);
     }
 
-    void resetBall(int id){
-        ball_x[id] = gc.Random(0,616);
-        ball_y[id] = -gc.Random(24,480);
-        ball_speed[id] = gc.Random(3,6);
-        ball_col[id] = gc.Random(1,3);
-        if(ball_col[id]==1){
+    void resetBall(int id)
+    {
+        ball_x[id] = gc.Random(0, 616);
+        ball_y[id] = -gc.Random(24, 480);
+        ball_speed[id] = gc.Random(3, 6);
+        ball_col[id] = gc.Random(1, 3);
+
+        if(ball_col[id] == 1)
+        {
             ball_img[id] = GcImage.BallYellow;
         }
-        else if(ball_col[id]==2){
+        else if(ball_col[id] == 2)
+        {
             ball_img[id] = GcImage.BallRed;
         }
-        else {
-            ball_img[id] = GcImage.BallBlue; 
+        else
+        {
+            ball_img[id] = GcImage.BallBlue;
         }
     }
 }
--- k04/Assets/GameCanvas/AutoGeneratedScripts/GcImage.cs
+++ k04/Assets/GameCanvas/AutoGeneratedScripts/GcImage.cs
@@ -13,11 +13,43 @@ namespace GameCanvas
 {
     public readonly partial struct GcImage : System.IEquatable<GcImage>
     {
-        internal const int __Length__ = 5;
+        internal const int __Length__ = 37;
         public static readonly GcImage BallBlue = new("BallBlue", 24, 24);
         public static readonly GcImage BallRed = new("BallRed", 24, 24);
         public static readonly GcImage BallYellow = new("BallYellow", 24, 24);
+        public static readonly GcImage BL0 = new("BL0", 32, 32);
+        public static readonly GcImage BL1 = new("BL1", 32, 32);
+        public static readonly GcImage BL2 = new("BL2", 32, 32);
+        public static readonly GcImage BL3 = new("BL3", 32, 32);
         public static readonly GcImage BlueSky = new("BlueSky", 640, 480);
+        public static readonly GcImage BR0 = new("BR0", 32, 32);
+        public static readonly GcImage BR1 = new("BR1", 32, 32);
+        public static readonly GcImage BR2 = new("BR2", 32, 32);
+        public static readonly GcImage BR3 = new("BR3", 32, 32);
+        public static readonly GcImage GL0 = new("GL0", 32, 32);
+        public static readonly GcImage GL1 = new("GL1", 32, 32);
+        public static readonly GcImage GL2 = new("GL2", 32, 32);
+        public static readonly GcImage GL3 = new("GL3", 32, 32);
+        public static readonly GcImage GR0 = new("GR0", 32, 32);
+        public static readonly GcImage GR1 = new("GR1", 32, 32);
+        public static readonly GcImage GR2 = new("GR2", 32, 32);
+        public static readonly GcImage GR3 = new("GR3", 32, 32);
         public static readonly GcImage MapPin = new("MapPin", 50, 64);
+        public static readonly GcImage RL0 = new("RL0", 32, 32);
+        public static readonly GcImage RL1 = new("RL1", 32, 32);
+        public static readonly GcImage RL2 = new("RL2", 32, 32);
+        public static readonly GcImage RL3 = new("RL3", 32, 32);
+        public static readonly GcImage RR0 = new("RR0", 32, 32);
+        public static readonly GcImage RR1 = new("RR1", 32, 32);
+        public static readonly GcImage RR2 = new("RR2", 32, 32);
+        public static readonly GcImage RR3 = new("RR3", 32, 32);
+        public static readonly GcImage YL0 = new("YL0", 32, 32);
+        public static readonly GcImage YL1 = new("YL1", 32, 32);
+        public static readonly GcImage YL2 = new("YL2", 32, 32);
+        public static readonly GcImage YL3 = new("YL3", 32, 32);
+        public static readonly GcImage YR0 = new("YR0", 32, 32);
+        public static readonly GcImage YR1 = new("YR1", 32, 32);
+        public static readonly GcImage YR2 = new("YR2", 32, 32);
+        public static readonly GcImage YR3 = new("YR3", 32, 32);
     }
 }
