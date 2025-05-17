aeb37fea42ffead50760b2047ce4510798522427
kwauso <akiranieda@keio.jp>
2025-05-17 12:41:26 +0900

    課題(4)まで完了

--- k04/Assets/Game.cs
+++ k04/Assets/Game.cs
@@ -20,15 +20,18 @@ public sealed class Game : GameBase
     int player_speed = 3;
     int player_w = 32;
     int player_h = 32;
-    GcImage player_img = GcImage.GR0;
 
     int score = 0;
     int time = 1800;
 
+    int player_dir = 1;
+    int player_col = 4;
+    int combo = 0;
+
     public override void InitGame()
     {
         gc.SetResolution(640, 480);
-        for(int i = 0; i < BALL_NUM; i++)
+        for (int i = 0; i < BALL_NUM; i++)
         {
             resetBall(i);
         }
@@ -41,10 +44,11 @@ public sealed class Game : GameBase
             time--;
         }
 
-        for(int i = 0; i < BALL_NUM; i++)
+        for (int i = 0; i < BALL_NUM; i++)
         {
             ball_y[i] += ball_speed[i];
-            if(ball_y[i] > 480)
+
+            if (ball_y[i] > 480)
             {
                 resetBall(i);
             }
@@ -54,23 +58,33 @@ public sealed class Game : GameBase
                 if (time > 0)
                 {
                     score += ball_col[i];
+                    if (player_col == ball_col[i])
+                    {
+                        combo++;
+                        score += combo;
+                    }
+                    else
+                    {
+                        combo = 0;
+                    }
+                    player_col = ball_col[i];
                 }
                 resetBall(i);
             }
         }
 
-        if(gc.GetPointerFrameCount(0) > 0)
+        if (gc.GetPointerFrameCount(0) > 0)
         {
             var px = gc.GetPointerX(0);
-            if(px > 320)
+            if (px > 320)
             {
                 player_x += player_speed;
-                player_img = GcImage.GR0;
+                player_dir = 1;
             }
             else
             {
                 player_x -= player_speed;
-                player_img = GcImage.GL0;
+                player_dir = -1;
             }
         }
     }
@@ -79,12 +93,19 @@ public sealed class Game : GameBase
     {
         gc.ClearScreen();
 
-        for(int i = 0; i < BALL_NUM; i++)
+        if (time > 0)
         {
-            gc.DrawImage(ball_img[i], ball_x[i], ball_y[i]);
-        }
+            for (int i = 0; i < BALL_NUM; i++)
+            {
+                gc.DrawImage(ball_img[i], ball_x[i], ball_y[i]);
+            }
 
-        gc.DrawImage(player_img, player_x, player_y);
+            drawCharacter(player_x, player_y, player_col, player_dir, (time % 60 >= 15) ? 2 : 1);
+        }
+        else
+        {
+            drawCharacter(player_x, player_y, player_col, player_dir, 3);
+        }
 
         gc.SetColor(0, 0, 0);
         if (time > 0)
@@ -96,6 +117,7 @@ public sealed class Game : GameBase
             gc.DrawString("finished!!", 0, 0);
         }
         gc.DrawString("score:" + score, 0, 24);
+        gc.DrawString("combo:" + combo, 0, 48);
     }
 
     void resetBall(int id)
@@ -105,11 +127,11 @@ public sealed class Game : GameBase
         ball_speed[id] = gc.Random(3, 6);
         ball_col[id] = gc.Random(1, 3);
 
-        if(ball_col[id] == 1)
+        if (ball_col[id] == 1)
         {
             ball_img[id] = GcImage.BallYellow;
         }
-        else if(ball_col[id] == 2)
+        else if (ball_col[id] == 2)
         {
             ball_img[id] = GcImage.BallRed;
         }
@@ -117,5 +139,25 @@ public sealed class Game : GameBase
         {
             ball_img[id] = GcImage.BallBlue;
         }
+    }
+
+    void drawCharacter(int x, int y, int col, int dir, int anim)
+    {
+        GcImage[] images = new[]
+        {
+            GcImage.YR0, GcImage.YR1, GcImage.YR2, GcImage.YR3,
+            GcImage.RR0, GcImage.RR1, GcImage.RR2, GcImage.RR3,
+            GcImage.BR0, GcImage.BR1, GcImage.BR2, GcImage.BR3,
+            GcImage.GR0, GcImage.GR1, GcImage.GR2, GcImage.GR3,
+            GcImage.YL0, GcImage.YL1, GcImage.YL2, GcImage.YL3,
+            GcImage.RL0, GcImage.RL1, GcImage.RL2, GcImage.RL3,
+            GcImage.BL0, GcImage.BL1, GcImage.BL2, GcImage.BL3,
+            GcImage.GL0, GcImage.GL1, GcImage.GL2, GcImage.GL3,
+        };
+
+        int img_num = (col - 1) * 4 + anim;
+        if (dir == -1) img_num += 16;
+
+        gc.DrawImage(images[img_num], x, y);
     }
 }
